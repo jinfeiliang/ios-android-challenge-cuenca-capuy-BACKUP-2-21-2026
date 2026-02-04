9a599c1210e2ae7fa1bbf8f81ecb5089086e2c8b
jinl33 <jinl33@nycstudents.net>
2026-02-04 13:55:25 -0500

    Testing Loading with number and better layout

--- src/js/script.js
+++ src/js/script.js
@@ -13,8 +13,14 @@ function print(msg){
 
 
 const Contacts_Container_DOM = document.querySelector("#Contacts-Container");
+const Contact_Selectors_DOM = document.querySelectorAll(".Contact_Selector");
 
 
+const Default_Profile_IMG = "https://cdn-icons-png.flaticon.com/512/9187/9187604.png";
+
+function Contact_Select(){
+
+}
 
 async function Get_All_Contacts() {
     try {
@@ -38,14 +44,24 @@ function Load_All_Contacts() {
     //console.log(Contacts_List);
 
     Promise_Contacts.then( (List) => {
-       let Contacts = List.Contacts;
+        let Contacts = List.Contacts;
 
         Contacts.forEach( (Contact) => {
-            
-            let HTML = `<div class="Contact">${Contact.name.first}${Contact.name.last}</div>`;
+            let Profile_IMG =  Contact.Profile;
+            console.log(Profile_IMG);
+            if (Profile_IMG == "" || Profile_IMG == undefined){
+                Profile_IMG = Default_Profile_IMG;
+            }
+            if (Contact.phone[0].number == undefined){
+                return console.log("Error trying load an contact that does not have an number");
+            }
+
+            let HTML = `<button class="Contact Contact_Selector onClick="Contact_Select(${Contact.phone[0].number})" ">
+            <img class="Contact_Profile_IMG" src="${Profile_IMG}">
+            <h1 class="Contact_Profile_Name">${Contact.name.first} ${Contact.name.last}</h1>
+            </button>`;
 
             Contacts_Container_DOM.insertAdjacentHTML("beforeend", HTML);
-
         })
 
     })
@@ -53,3 +69,10 @@ function Load_All_Contacts() {
 }
 
 Load_All_Contacts();
+
+
+
+
+
+
+
