98442829c6128a5c84f55237c29e2dd59bac0028
tinfeek80 <jinl33@nycstudents.net>
2026-02-03 21:08:56 -0500

    Testing Loading

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,22 @@
 
+let Debug_Mode = true;
+function print(msg){
+    if (Debug_Mode){
+        console.log("-----------------------")
+        console.log("Caller:");
+        console.log(print.caller)
+        console.log("Msg:")
+        console.log(msg);
+        console.log("-----------------------")
+    }
+}
+
+
+const Contacts_Container_DOM = document.querySelector("#Contacts-Container");
 
 
 
-
-
-
-async function test() {
+async function Get_All_Contacts() {
     try {
         const response = await fetch("../src/data/api.json");
         if (!response.ok){
@@ -13,10 +24,32 @@ async function test() {
         }
 
         const result = await response.json();
-        console.log(result);
-
+        //console.log(result);
+        return result;
     } catch (error) {
         console.log(error.message);
     }
 }
-test();
+
+
+function Load_All_Contacts() {
+
+    let Promise_Contacts = Get_All_Contacts();
+    //console.log(Contacts_List);
+
+    Promise_Contacts.then( (List) => {
+       let Contacts = List.Contacts;
+
+        Contacts.forEach( (Contact) => {
+            
+            let HTML = `<div class="Contact">${Contact.name.first}${Contact.name.last}</div>`;
+
+            Contacts_Container_DOM.insertAdjacentHTML("beforeend", HTML);
+
+        })
+
+    })
+
+}
+
+Load_All_Contacts();
