2494756e4b0e5406ad5fe7c98e9c77ffdc1a5125
tinfeek80 <jinl33@nycstudents.net>
2026-02-17 18:33:39 -0500

    Edit Information Mobile View

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -3,6 +3,7 @@ let Mobile_Width = 630;
 let Contacts_Section = document.querySelector(".Contacts_Section");
 let Information_Section = document.querySelector(".Information_Section");
 let Return_To_Main_BTN = document.querySelector("#Return_To_Main_BTN");
+let Edit_Cancel_BTN = document.querySelector("#Edit_Cancel_BTN");
 
 let Current_Width = window.innerWidth;
 
@@ -15,7 +16,6 @@ function Switch_To_Mobile_Info() {
   console.log("Switching to Mobile Info");
   Information_Section.scrollIntoView({ behavior: "smooth" });
 }
-
 function Switch_To_Contacts() {
   //console.log(Current_Width);
   Current_Width = window.innerWidth;
@@ -27,6 +27,8 @@ function Switch_To_Contacts() {
   Contacts_Section.scrollIntoView({ behavior: "smooth" });
 }
 Return_To_Main_BTN.addEventListener("click", Switch_To_Contacts);
+
+Edit_Cancel_BTN.addEventListener("click", Switch_To_Mobile_Info);
 
 let resizerTimer;
 
